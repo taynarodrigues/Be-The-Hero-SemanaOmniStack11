c8ca4bbe8483f15a4c79af294fe9bbccbaf5f42a
Tayna Rodrigues <taynajesus@gmail.com>
2020-03-25 12:17:47 -0400

    Route Params: Parâmetros utilizados para identificar recursos

--- aulas/backend/index.js
+++ aulas/backend/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/users', (request, response) => {
-	const params = request.query;
+app.get('/users/:id', (request, response) => {
+	const params = request.params;
 
 	console.log(params);
 
