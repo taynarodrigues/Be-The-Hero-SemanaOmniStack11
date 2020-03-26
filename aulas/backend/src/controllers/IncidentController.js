547a2f6a9de82e4182e54135bf7841fdc53189ed
Tayna Rodrigues <taynajesus@gmail.com>
2020-03-25 21:48:19 -0400

    Finalizando a construção do back-end. Além de adicionar o  módulo CORS; Junto das demais modificações de entidades e funcionalidades e o gitignore.

--- aulas/backend/src/controllers/IncidentController.js
+++ aulas/backend/src/controllers/IncidentController.js
@@ -2,12 +2,28 @@ const connection = require('../database/connection');
 
 module.exports = {
 	async index(request, response) { // método index para fazer a listagem 
-			const { page = 1 } = request.query; 	//Buscar de dentro do request.query um parâmtro "page"
+			const { page = 1 } = request.query;//Buscar de dentro do request.query um parâmtro "page"
+
+			const [count] = await connection('incidents') //retornar a quantidade de todos os casos
+			.count(); // conta todos os campos da tabela
+			console.log(count);
 
 			const incidents = await connection('incidents') //Trazendo todos os dados do incidents
+			.join('ongs', 'ongs.id', '=', 'incidents.ong_id')//relacionar dados de duas tabelas
 			.limit(5) // limitar a busca para 5 registro
 			.offset((page - 1) * 5) //Na página preciso pular zero.Pular 5 registros por página (0 * 5)
-			.select('*');
+			.select([
+				'incidents.*', //Selecionar através de um array: todos os dados do incidentes e selecionar os demais campos o qual quero
+				'ongs.name',
+				'ongs.email',
+				'ongs.whatsapp',
+				'ongs.city',
+				'ongs.uf'
+		]);
+
+
+			response.header('X-Total-Count', count['count(*)']) //retornar o número de regitros
+
 			return response.json(incidents);
 			},
 
