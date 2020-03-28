1287b52e8afcccc645e8510359f234dad4fb4945
Tayna Rodrigues <taynajesus@gmail.com>
2020-03-27 21:16:03 -0400

    Conceito de 'Estado e Imutabilidade

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState} from 'react'; //importar 'useState' para converter o código usando um estado
 
 import Header from './Header';
 
-function App() {
-  return(
-    // customizando utilizando a propriedade title 
-    // <Header title = "Semana OmniStack" />  
-    
+function App(){
 
-    //retorna todo o conteúdo do texto 'Semana OmniStack' através da propriedade children de forma automática,
-    <Header>
-      Semana OmniStack
-    </Header>
+    /*Estado:
+        O método useState, retorna um array com duas posições
+        Array [valor, funcaoDeAtualizacao]*/
+    /*Imutabilidade:
+         Dentro do React por questão de performance nunca pode manipular a váriavel do estado,
+         o valor do estado de uma forma direta, não pode alterar o valor de uma forma direta,
+         para isso precisase sobrepor a váriavel do estado*/
+  
+  const [counter, setCounter] = useState(0); 
+
+  function increment(){
+    setCounter(counter + 1); // o valor anterior + 1.
+    console.log(counter);
+  }
+
+  return(
+    /*Toda vez q o componente armazenar uma informação dentro dele, criaremos sempre um estado
+    pois assim consegue-se atualiar a informação e reflete dentro da interface*/ 
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button> 
+    </div>
   );
 }
 
