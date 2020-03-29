22724de4eb3b74b7d159909ad245b6f22d0b9697
Tayna Rodrigues <taynajesus@gmail.com>
2020-03-28 22:50:05 -0400

    Criando projeto React Native

---
+++ aulas/mobile/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello OmniStack</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  //estilização própria
+  title:{
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+});
