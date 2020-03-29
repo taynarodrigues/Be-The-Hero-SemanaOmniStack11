710248f0acceb5e81835b3f26ef22b10af2b6078
Tayna Rodrigues <taynajesus@gmail.com>
2020-03-29 00:47:27 -0400

    Configurando a navegação e a Página de casos

--- aulas/mobile/App.js
+++ aulas/mobile/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello OmniStack</Text>
-    </View>
+    <Routes/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  //estilização própria
-  title:{
-    color: '#fff',
-    fontSize: 20,
-    fontWeight: 'bold'
-  },
-});
---
+++ aulas/mobile/src/pages/Incidents/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
+
+import logoImg from '../../assets/logo.png'
+
+import styles from './styles';
+
+export default function Incidents(){
+    const navigation = useNavigation();
+
+    function navigationToDetail(){
+        navigation.navigate('Detail');
+    }
+
+
+    return(
+    <View style={styles.container}>
+        <View style={ styles.header}>
+        <Image source={logoImg}/>
+        <Text style={styles.headerText}>
+            Total de <Text style={styles.headerTextBold}>0 casos.</Text>
+        </Text>
+    </View>
+    
+    <Text style={styles.title}>Bem-vindo!</Text>
+    <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
+
+    <FlatList
+        data={[ 1, 2, 3, 4, 5]}
+        style={styles.incidentList}
+        keyExtractor={incident => String(incident)}
+        showsVerticalScrollIndicator={false} // tirar a visualização do scroll
+        renderItem={() => (
+        <View style={styles.incident}>
+         <Text style={styles.incidentProperty}>ONG:</Text>
+         <Text style={styles.incidentValue}>APAD</Text>
+
+         <Text style={styles.incidentProperty}>CASO:</Text>
+         <Text style={styles.incidentValue}>Cadelinha atroplada</Text>
+
+         <Text style={styles.incidentProperty}>VALOR:</Text>
+         <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+         <TouchableOpacity
+            style={styles.detailsButton}
+            onPress={navigationToDetail}
+        >
+          <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+          <Feather name="arrow-right" size={16} color="E02041"/>
+        </TouchableOpacity>
+        </View>
+      )}
+     />  
+    </View>  
+);
+}
